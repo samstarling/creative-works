4badf768aa8c63c852d870ba08e47b03a40cdd4a
Sam Starling <sam.starling@gmail.com>
2013-06-27 17:31:25 +0100

    Rephrasing specs

--- spec/navigation_spec.rb
+++ spec/navigation_spec.rb
@@ -1,13 +1,13 @@
 require_relative 'spec_helper'
 
 describe Navigation do
-  it "should return all items" do
+  it "returns all items" do
     nav_item = NavigationItem.new "Tennis", "/tennis"
     nav = Navigation.new([nav_item])
     nav.items.should == [nav_item]
   end
   
-  it "should return items in their correct order" do
+  it "returns items in their correct order" do
     first_item = NavigationItem.new "Tennis", "/tennis", 1
     second_item = NavigationItem.new "Football", "/football", 2
     nav = Navigation.new([second_item, first_item])
@@ -16,7 +16,7 @@ describe Navigation do
 end
 
 describe NavigationItem do
-  it "should be active if the request path matches" do
+  it "is deemed active when the request path matches" do
     request = double("Request")
     request.stub(:path) { "/tennis" }
     nav_item = NavigationItem.new "Tennis", "/tennis"
