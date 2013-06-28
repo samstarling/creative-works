0c546418d100bea5cd9cbcf845a0c462184c0d49
Sam Starling <sam.starling@gmail.com>
2013-06-28 14:33:23 +0100

    TagConcept object now parses out basic fields

--- lib/tag_concept.rb
+++ lib/tag_concept.rb
@@ -1,5 +1,19 @@
+require_relative 'json_helper'
+
 class TagConcept
   def initialize json
     @json = json
+  end
+  
+  def uri
+    @json['@id']
+  end
+  
+  def type
+    JSONHelper.normalize_array(@json['@type'])
+  end
+  
+  def label
+    @json['label']
   end
 end
---
+++ spec/tag_concept_spec.rb
@@ -0,0 +1,15 @@
+require_relative 'spec_helper'
+
+describe TagConcept do
+  it "parses basic tag concept fields" do
+    tag_concept = TagConcept.new json_fixture("tag_concept/no_label.json")
+    tag_concept.uri.should == "http://sws.geonames.org/3333203/"
+    tag_concept.type.should == ["Thing", "TagConcept"]
+    tag_concept.label.should == nil
+  end
+  
+  it "also parses tag concepts with labels" do
+    tag_concept = TagConcept.new json_fixture("tag_concept/simple.json")
+    tag_concept.label.should == "Stoke v Liverpool"
+  end
+end
